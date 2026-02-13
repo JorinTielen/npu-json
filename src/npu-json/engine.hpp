ee995a6df7a1ce4bf9fb7f4b7c16daad16613b60
CuriousLocky <835322750jm37@outlook.com>
2026-02-13 23:40:35 +0000

    Change chunk size to 8MB

--- src/npu-json/engine.hpp
+++ src/npu-json/engine.hpp
@@ -57,7 +57,8 @@ class Engine {
 public:
   // Must be kept in sync with the DATA_CHUNK_SIZE AND DATA_BLOCK_SIZE in `src/aie/gen_mlir_design.py`.
   static constexpr size_t BLOCK_SIZE = 16 * 1024;
-  static constexpr size_t CHUNK_SIZE = 1 * 1024 * BLOCK_SIZE;
+  static constexpr size_t BLOCKS_PER_CHUNK = 512;
+  static constexpr size_t CHUNK_SIZE = BLOCKS_PER_CHUNK * BLOCK_SIZE;
 
   Engine(jsonpath::Query &query, std::string_view json);
   ~Engine();
