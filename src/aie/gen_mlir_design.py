b5cfa47e9dc55cf3cd1a9c4147dbacc19da46a1d
CuriousLocky <835322750jm37@outlook.com>
2026-02-14 21:10:39 +0000

    Add build options for both supported hw

--- src/aie/gen_mlir_design.py
+++ src/aie/gen_mlir_design.py
@@ -12,38 +12,51 @@ from aie.extras.context import mlir_mod_ctx
 # from aie.helpers.dialects.ext.scf import _for as range_
 
 
-# Must be kept in sync with the CHUNK_SIZE and BLOCK_SIZE in `src/npu-json/engine.hpp`.
-# The 4 extra bytes are for the carry index
-BLOCKS_PER_CHUNK = 512
-DATA_BLOCK_SIZE = 16 * 1024
-DATA_CHUNK_SIZE = BLOCKS_PER_CHUNK * DATA_BLOCK_SIZE
-
-INDEX_CHUNK_SIZE = DATA_CHUNK_SIZE // 8
-INDEX_BLOCK_SIZE = DATA_BLOCK_SIZE // 8
-
-CARRY_CHUNK_SIZE = DATA_CHUNK_SIZE // DATA_BLOCK_SIZE * 4
-CARRY_BLOCK_SIZE = 4
-
-# AI Engine structural design function
-def aie_design(kernel_obj: str):
-    num_cols = 8
+def aie_design(
+    kernel_obj: str,
+    block_size: int,
+    blocks_per_chunk: int,
+    num_cols: int,
+    aie_device: AIEDevice,
+):
     num_rows = 4
 
+    data_block_size = block_size
+    data_chunk_size = blocks_per_chunk * data_block_size
+
+    index_chunk_size = data_chunk_size // 8
+    index_block_size = data_block_size // 8
+
+    carry_chunk_size = data_chunk_size // data_block_size * 4
+    carry_block_size = 4
+
     # Device declaration - aie2 device NPU
-    @device(AIEDevice.npu2)
+    @device(aie_device)
     def device_body():
-        data_chunk_ty = np.ndarray[(DATA_CHUNK_SIZE,), np.dtype[np.uint8]]
-        data_block_ty = np.ndarray[(DATA_BLOCK_SIZE,), np.dtype[np.uint8]]
-        data_split_ty = np.ndarray[((DATA_BLOCK_SIZE) * (num_rows // 2),), np.dtype[np.uint8]]
-        string_chunk_ty = np.ndarray[(INDEX_CHUNK_SIZE * 2 + CARRY_CHUNK_SIZE,), np.dtype[np.uint8]]
-        string_block_ty = np.ndarray[(INDEX_BLOCK_SIZE * 2 + CARRY_BLOCK_SIZE,), np.dtype[np.uint8]]
-        string_split_ty = np.ndarray[((INDEX_BLOCK_SIZE * 2 + CARRY_BLOCK_SIZE) * (num_rows // 2),), np.dtype[np.uint8]]
-        index_chunk_ty = np.ndarray[(INDEX_CHUNK_SIZE,), np.dtype[np.uint8]]
-        index_block_ty = np.ndarray[(INDEX_BLOCK_SIZE,), np.dtype[np.uint8]]
-        index_split_ty = np.ndarray[(INDEX_BLOCK_SIZE * (num_rows // 2),), np.dtype[np.uint8]]
+        data_chunk_ty = np.ndarray[(data_chunk_size,), np.dtype[np.uint8]]
+        data_block_ty = np.ndarray[(data_block_size,), np.dtype[np.uint8]]
+        data_split_ty = np.ndarray[
+            ((data_block_size) * (num_rows // 2),), np.dtype[np.uint8]
+        ]
+        string_chunk_ty = np.ndarray[
+            (index_chunk_size * 2 + carry_chunk_size,), np.dtype[np.uint8]
+        ]
+        string_block_ty = np.ndarray[
+            (index_block_size * 2 + carry_block_size,), np.dtype[np.uint8]
+        ]
+        string_split_ty = np.ndarray[
+            ((index_block_size * 2 + carry_block_size) * (num_rows // 2),),
+            np.dtype[np.uint8],
+        ]
+        index_chunk_ty = np.ndarray[(index_chunk_size,), np.dtype[np.uint8]]
+        index_block_ty = np.ndarray[(index_block_size,), np.dtype[np.uint8]]
+        index_split_ty = np.ndarray[
+            (index_block_size * (num_rows // 2),), np.dtype[np.uint8]
+        ]
 
         tiles = [
-            [tile(col, row) for col in range(0, num_cols)] for row in range(0, num_rows + 2)
+            [tile(col, row) for col in range(0, num_cols)]
+            for row in range(0, num_rows + 2)
         ]
         shim_tiles = tiles[0]
         mem_tiles = tiles[1]
@@ -54,14 +67,13 @@ def aie_design(kernel_obj: str):
         )
 
         structural_kernel = external_func(
-            "structural_character_index", inputs=[data_block_ty, index_block_ty, np.int32]
+            "structural_character_index",
+            inputs=[data_block_ty, index_block_ty, np.int32],
         )
 
         shim_fifos_in_string = [None] * num_cols
         shim_fifos_in_structural = [None] * num_cols
-        core_fifos_in = [
-            [None for _ in range(0, num_cols)] for _ in range(0, num_rows)
-        ]
+        core_fifos_in = [[None for _ in range(0, num_cols)] for _ in range(0, num_rows)]
 
         shim_fifos_out_string = [None] * num_cols
         shim_fifos_out_structural = [None] * num_cols
@@ -76,7 +88,7 @@ def aie_design(kernel_obj: str):
                 shim_tiles[col],
                 mem_tiles[col],
                 2,
-                string_split_ty
+                string_split_ty,
             )
             for row in range(0, num_rows // 2):
                 core_fifos_in[row][col] = object_fifo(
@@ -84,13 +96,16 @@ def aie_design(kernel_obj: str):
                     mem_tiles[col],
                     core_tiles[row][col],
                     2,
-                    string_block_ty
+                    string_block_ty,
                 )
             object_fifo_link(
                 shim_fifos_in_string[col],
                 [core_fifos_in[row][col] for row in range(0, num_rows // 2)],
                 [],
-                [i * (INDEX_BLOCK_SIZE * 2 + CARRY_BLOCK_SIZE) for i in range(0, num_rows // 2)],
+                [
+                    i * (index_block_size * 2 + carry_block_size)
+                    for i in range(0, num_rows // 2)
+                ],
             )
 
         # Setup FIFOs for structural kernel input data (second half of rows)
@@ -100,7 +115,7 @@ def aie_design(kernel_obj: str):
                 shim_tiles[col],
                 mem_tiles[col],
                 2,
-                data_split_ty
+                data_split_ty,
             )
             for row in range(num_rows // 2, num_rows):
                 core_fifos_in[row][col] = object_fifo(
@@ -108,13 +123,13 @@ def aie_design(kernel_obj: str):
                     mem_tiles[col],
                     core_tiles[row][col],
                     2,
-                    data_block_ty
+                    data_block_ty,
                 )
             object_fifo_link(
                 shim_fifos_in_structural[col],
                 [core_fifos_in[row][col] for row in range(num_rows // 2, num_rows)],
                 [],
-                [i * DATA_BLOCK_SIZE for i in range(0, num_rows // 2)],
+                [i * data_block_size for i in range(0, num_rows // 2)],
             )
 
         # Setup FIFOs for string kernel output (first half of rows)
@@ -124,7 +139,7 @@ def aie_design(kernel_obj: str):
                 mem_tiles[col],
                 shim_tiles[col],
                 2,
-                index_split_ty
+                index_split_ty,
             )
             for row in range(0, num_rows // 2):
                 core_fifos_out[row][col] = object_fifo(
@@ -132,13 +147,13 @@ def aie_design(kernel_obj: str):
                     core_tiles[row][col],
                     mem_tiles[col],
                     2,
-                    index_block_ty
+                    index_block_ty,
                 )
             object_fifo_link(
                 [core_fifos_out[row][col] for row in range(0, num_rows // 2)],
                 shim_fifos_out_string[col],
-                [i * INDEX_BLOCK_SIZE for i in range(0, num_rows // 2)],
-                []
+                [i * index_block_size for i in range(0, num_rows // 2)],
+                [],
             )
 
         # Setup FIFOs for structural kernel output (second half of rows)
@@ -148,7 +163,7 @@ def aie_design(kernel_obj: str):
                 mem_tiles[col],
                 shim_tiles[col],
                 2,
-                index_split_ty
+                index_split_ty,
             )
             for row in range(num_rows // 2, num_rows):
                 core_fifos_out[row][col] = object_fifo(
@@ -156,58 +171,79 @@ def aie_design(kernel_obj: str):
                     core_tiles[row][col],
                     mem_tiles[col],
                     2,
-                    index_block_ty
+                    index_block_ty,
                 )
             object_fifo_link(
                 [core_fifos_out[row][col] for row in range(num_rows // 2, num_rows)],
                 shim_fifos_out_structural[col],
-                [i * INDEX_BLOCK_SIZE for i in range(0, num_rows // 2)],
-                []
+                [i * index_block_size for i in range(0, num_rows // 2)],
+                [],
             )
 
         # Basic validation of data sizes to avoid hard-crashing the NPU.
-        assert (DATA_CHUNK_SIZE / DATA_BLOCK_SIZE / num_cols / num_rows).is_integer(), \
+        assert (data_chunk_size / data_block_size / num_cols / num_rows).is_integer(), (
             "Data sizes do not evenly divide for splitting across tiles"
-        assert (INDEX_CHUNK_SIZE / INDEX_BLOCK_SIZE / num_cols / num_rows).is_integer(), \
-            "Index sizes do not evenly divide for splitting across tiles"
+        )
+        assert (
+            index_chunk_size / index_block_size / num_cols / num_rows
+        ).is_integer(), "Index sizes do not evenly divide for splitting across tiles"
 
         # Compute tile core definitions for running the string kernel (first half of rows)
         for col in range(0, num_cols):
             for row in range(0, num_rows // 2):
+
                 @core(core_tiles[row][col], kernel_obj)
                 def core_body():
                     for _ in range_(0, sys.maxsize):
-                        for _ in range_(DATA_CHUNK_SIZE // DATA_BLOCK_SIZE // num_cols // (num_rows // 2)):
+                        for _ in range_(
+                            data_chunk_size
+                            // data_block_size
+                            // num_cols
+                            // (num_rows // 2)
+                        ):
                             of_in = core_fifos_in[row][col]
                             of_out = core_fifos_out[row][col]
 
                             elem_in = of_in.acquire(ObjectFifoPort.Consume, 1)
                             elem_out = of_out.acquire(ObjectFifoPort.Produce, 1)
-                            string_kernel(elem_in, elem_out, DATA_BLOCK_SIZE)
+                            string_kernel(elem_in, elem_out, data_block_size)
                             of_in.release(ObjectFifoPort.Consume, 1)
                             of_out.release(ObjectFifoPort.Produce, 1)
 
         # Compute tile core definitions for running the structuralkernel (second half of rows)
         for col in range(0, num_cols):
             for row in range(num_rows // 2, num_rows):
+
                 @core(core_tiles[row][col], kernel_obj)
                 def core_body():
                     for _ in range_(0, sys.maxsize):
-                        for _ in range_(DATA_CHUNK_SIZE // DATA_BLOCK_SIZE // num_cols // (num_rows // 2)):
+                        for _ in range_(
+                            data_chunk_size
+                            // data_block_size
+                            // num_cols
+                            // (num_rows // 2)
+                        ):
                             of_in = core_fifos_in[row][col]
                             of_out = core_fifos_out[row][col]
 
                             elem_in = of_in.acquire(ObjectFifoPort.Consume, 1)
                             elem_out = of_out.acquire(ObjectFifoPort.Produce, 1)
-                            structural_kernel(elem_in, elem_out, DATA_BLOCK_SIZE)
+                            structural_kernel(elem_in, elem_out, data_block_size)
                             of_in.release(ObjectFifoPort.Consume, 1)
                             of_out.release(ObjectFifoPort.Produce, 1)
 
         # Host side data-flow movement
-        @runtime_sequence(data_chunk_ty, string_chunk_ty, index_chunk_ty, index_chunk_ty)
-        def sequence(data_buffer, string_input_buffer, string_index_buffer, structural_index_buffer):
+        @runtime_sequence(
+            data_chunk_ty, string_chunk_ty, index_chunk_ty, index_chunk_ty
+        )
+        def sequence(
+            data_buffer,
+            string_input_buffer,
+            string_index_buffer,
+            structural_index_buffer,
+        ):
             for col in range(0, num_cols):
-                string_input_chunk_size = INDEX_CHUNK_SIZE * 2 + CARRY_CHUNK_SIZE
+                string_input_chunk_size = index_chunk_size * 2 + carry_chunk_size
                 npu_dma_memcpy_nd(
                     metadata=shim_fifos_in_string[col],
                     # bd_id=col * 4,
@@ -215,34 +251,34 @@ def aie_design(kernel_obj: str):
                     mem=string_input_buffer,
                     sizes=[1, 1, 1, string_input_chunk_size // num_cols],
                     offsets=[0, 0, 0, (string_input_chunk_size // num_cols) * col],
-                    issue_token=True
+                    issue_token=True,
                 )
                 npu_dma_memcpy_nd(
                     metadata=shim_fifos_out_string[col],
                     # bd_id=col * 4 + 1,
                     bd_id=1,
                     mem=string_index_buffer,
-                    sizes=[1, 1, 1, INDEX_CHUNK_SIZE // num_cols],
-                    offsets=[0, 0, 0, (INDEX_CHUNK_SIZE // num_cols) * col],
-                    issue_token=True
+                    sizes=[1, 1, 1, index_chunk_size // num_cols],
+                    offsets=[0, 0, 0, (index_chunk_size // num_cols) * col],
+                    issue_token=True,
                 )
                 npu_dma_memcpy_nd(
                     metadata=shim_fifos_in_structural[col],
                     # bd_id=col * 4 + 2,
                     bd_id=2,
                     mem=data_buffer,
-                    sizes=[1, 1, 1, DATA_CHUNK_SIZE // num_cols],
-                    offsets=[0, 0, 0, (DATA_CHUNK_SIZE // num_cols) * col],
-                    issue_token=True
+                    sizes=[1, 1, 1, data_chunk_size // num_cols],
+                    offsets=[0, 0, 0, (data_chunk_size // num_cols) * col],
+                    issue_token=True,
                 )
                 npu_dma_memcpy_nd(
                     metadata=shim_fifos_out_structural[col],
                     # bd_id=col * 4 + 3,
                     bd_id=3,
                     mem=structural_index_buffer,
-                    sizes=[1, 1, 1, INDEX_CHUNK_SIZE // num_cols],
-                    offsets=[0, 0, 0, (INDEX_CHUNK_SIZE // num_cols) * col],
-                    issue_token=True
+                    sizes=[1, 1, 1, index_chunk_size // num_cols],
+                    offsets=[0, 0, 0, (index_chunk_size // num_cols) * col],
+                    issue_token=True,
                 )
             dma_wait(*shim_fifos_out_string)
             dma_wait(*shim_fifos_out_structural)
@@ -252,17 +288,46 @@ def main():
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("--kernel-obj", type=str, help="kernel object file")
     arg_parser.add_argument("--out", type=str, help="output file", default="aie.mlir")
+    arg_parser.add_argument(
+        "--block-size", type=int, default=16 * 1024, help="input block size in bytes"
+    )
+    arg_parser.add_argument(
+        "--blocks-per-chunk", type=int, default=512, help="blocks per chunk"
+    )
+    arg_parser.add_argument(
+        "--num-cols", type=int, default=8, help="number of NPU columns"
+    )
+    arg_parser.add_argument(
+        "--aie-device",
+        type=str,
+        default="npu2",
+        choices=["npu1", "npu2"],
+        help="AIE device",
+    )
     args = arg_parser.parse_args()
 
+    aie_device_map = {
+        "npu1": AIEDevice.npu1,
+        "npu2": AIEDevice.npu2,
+    }
+
+    aie_device = aie_device_map[args.aie_device]
+
     with mlir_mod_ctx() as ctx:
-        aie_design(args.kernel_obj)
+        aie_design(
+            args.kernel_obj,
+            args.block_size,
+            args.blocks_per_chunk,
+            args.num_cols,
+            aie_device,
+        )
         res = ctx.module.operation.verify()
         if not res:
             print(res, sys.stderr)
             exit(1)
-        with open(args.out, 'w') as f:
+        with open(args.out, "w") as f:
             f.write(str(ctx.module))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
---
+++ src/aie/json_xdna1.cc
@@ -0,0 +1,126 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <aie_api/aie.hpp>
+
+#ifndef __STRINGIFY
+#define __STRINGIFY(a) #a
+#endif
+#define AIE_LOOP_MIN_ITERATION_COUNT(x)                                        \
+  _Pragma(__STRINGIFY(clang loop min_iteration_count(x)))
+#define AIE_LOOP_MAX_ITERATION_COUNT(x)                                        \
+  _Pragma(__STRINGIFY(clang loop max_iteration_count(x)))
+#define AIE_LOOP_RANGE(a, ...)                                                 \
+  AIE_LOOP_MIN_ITERATION_COUNT(a)                                              \
+  __VA_OPT__(AIE_LOOP_MAX_ITERATION_COUNT(__VA_ARGS__))
+#define AIE_PREPARE_FOR_PIPELINING
+
+__attribute__((inline)) uint64_t prefix_xor(uint64_t bitmask) {
+    bitmask ^= bitmask << 1;
+    bitmask ^= bitmask << 2;
+    bitmask ^= bitmask << 4;
+    bitmask ^= bitmask << 8;
+    bitmask ^= bitmask << 16;
+    bitmask ^= bitmask << 32;
+    return bitmask;
+}
+
+// TODO: Refactor to use structs and std::array for input/output buffers instead of byte pointers.
+
+void string_index_aie(uint8_t *__restrict in_buffer, uint64_t *__restrict index_buffer, const int32_t n) {
+  const size_t index_size = n / 8;
+  uint64_t *__restrict quotes_ptr = (uint64_t *)in_buffer;
+  uint64_t *__restrict backslash_ptr = (uint64_t *)(in_buffer + index_size);
+  uint32_t *__restrict carry_ptr = (uint32_t *)(in_buffer + index_size * 2);
+
+  static constexpr unsigned int V = 64;
+
+  static constexpr const uint64_t ODD_BITS = 0xAAAAAAAAAAAAAAAAULL;
+
+  uint64_t prev_in_string = 0;
+  uint64_t prev_is_escaped = uint64_t(*carry_ptr);
+
+  AIE_PREPARE_FOR_PIPELINING
+  for (unsigned int i = 0; i < n; i += V) {
+    // Scan for quote and escape characters in input
+    uint64_t quotes = *quotes_ptr++;
+    uint64_t backslash = *backslash_ptr++;
+
+    // Find odd-length sequences of escape characters (Fig. 3)
+    uint64_t potential_escape = backslash & ~prev_is_escaped;
+    uint64_t maybe_escaped = potential_escape << 1;
+
+    uint64_t maybe_escaped_and_odd_bits     = maybe_escaped | ODD_BITS;
+    uint64_t even_series_codes_and_odd_bits = maybe_escaped_and_odd_bits - potential_escape;
+
+    uint64_t escape_and_terminal_code = even_series_codes_and_odd_bits ^ ODD_BITS;
+    uint64_t escaped = escape_and_terminal_code ^ (backslash | prev_is_escaped);
+    uint64_t escape = escape_and_terminal_code & backslash;
+    prev_is_escaped = escape >> 63;
+
+    // Compute string-index (Fig. 4)
+    uint64_t non_escaped_quotes = quotes & ~escaped;
+    uint64_t string_index = prefix_xor(non_escaped_quotes);
+
+    // Invert if we were in a string previously
+    string_index = string_index ^ prev_in_string;
+    prev_in_string = static_cast<int64_t>(string_index) >> 63;
+
+    // Store result
+    *index_buffer++ = string_index;
+  }
+}
+
+void structural_character_index_aie(
+  uint8_t *__restrict in_buffer,
+  uint64_t *__restrict index_buffer,
+  const int32_t n
+) {
+  static constexpr unsigned int V = 64;
+
+  uint8_t *__restrict data_ptr = (uint8_t *)in_buffer;
+
+  const aie::vector<uint8_t, V> brace_open_mask = aie::broadcast<uint8_t, V>('{');
+  const aie::vector<uint8_t, V> brace_close_mask = aie::broadcast<uint8_t, V>('}');
+
+  const aie::vector<uint8_t, V> bracket_open_mask = aie::broadcast<uint8_t, V>('[');
+  const aie::vector<uint8_t, V> bracket_close_mask = aie::broadcast<uint8_t, V>(']');
+
+  const aie::vector<uint8_t, V> colon_mask = aie::broadcast<uint8_t, V>(':');
+  const aie::vector<uint8_t, V> comma_mask = aie::broadcast<uint8_t, V>(',');
+
+  AIE_PREPARE_FOR_PIPELINING
+  for (size_t i = 0; i < n; i += V) {
+    const aie::vector<uint8_t, V> data = aie::load_v<V>(data_ptr);
+    data_ptr += V;
+
+    auto brace_open = aie::eq(data, brace_open_mask).to_uint64();
+    auto brace_close = aie::eq(data, brace_close_mask).to_uint64();
+    auto braces = brace_open | brace_close;
+
+    auto bracket_open = aie::eq(data, bracket_open_mask).to_uint64();
+    auto bracket_close = aie::eq(data, bracket_close_mask).to_uint64();
+    auto brackets = bracket_open | bracket_close;
+
+    auto colon = aie::eq(data, colon_mask).to_uint64();
+    auto comma = aie::eq(data, comma_mask).to_uint64();
+    auto colons_and_commas = colon | comma;
+
+    auto structurals = braces | brackets | colons_and_commas;
+
+    *index_buffer++ = structurals;
+  }
+}
+
+extern "C" {
+
+void string_index(uint8_t *in_buffer, uint64_t *index_buffer, int32_t n) {
+  string_index_aie(in_buffer, index_buffer, n);
+}
+
+void structural_character_index(uint8_t *in_buffer, uint64_t *index_buffer, int32_t n) {
+  structural_character_index_aie(in_buffer, index_buffer, n);
+}
+
+}
