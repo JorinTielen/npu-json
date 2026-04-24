2b078138dd1aebc96cdc44b91211296253db196d
CuriousLocky <835322750jm37@outlook.com>
2026-04-24 15:19:12 +0200

    Fix matrix insts path to match aiecc output location

--- src/npu-json/options.hpp
+++ src/npu-json/options.hpp
@@ -6,4 +6,4 @@ const std::string XCLBIN_PATH = "build/src/aie/json.xclbin";
 const std::string INSTS_PATH  = "build/insts.txt";
 
 const std::string MATRIX_XCLBIN_PATH = "build/src/aie/json_matrix.xclbin";
-const std::string MATRIX_INSTS_PATH   = "build/src/aie/json_matrix_insts.txt";
+const std::string MATRIX_INSTS_PATH   = "build/json_matrix_insts.txt";
